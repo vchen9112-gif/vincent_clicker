4a41f5005b08f97b555052b378ff5dd900d09aeb
Vincent Chen <vchen9112@gmail.com>
2026-03-11 13:08:56 -0400

    Refactor counter app with localStorage functionality

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-//Counter APP
-documents.getElementById("count-el").innerText=5
+// counter app
+function init(){
+    counts_str = localStorage.getItem("count")
+    count = counts_str ? parse_latest_count() :  0
+    display()
+}
+function increment(){
+    count++
+    display()
+}
+function display(){
+    document.getElementById("count-el").innerText = count
+    document.getElementById("count-past").innerText = counts_str
+}
+function save(){
+    counts_str = counts_str? `${counts_str} - ${count}` : `${count}`
+    localStorage.setItem("count", counts_str)
+    display()
+}
+function clr(){
+    localStorage.removeItem("count")
+    counts_str = ""
+    count = 0
+    display()
+}
+function parse_latest_count(){
+    count_arr = counts_str.split(" - ")
+    return count_arr[count_arr.length - 1]
+}
